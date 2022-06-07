e9945a2038877964152f9809252a8a951b01983b
Elyorbek-2 <elyorbekboyxonov@gmail.com>
2022-06-07 10:34:30 +0500

    create project

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    static boolean isActive =true;
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("1.Sign In");
+        System.out.println("2.Sign Up");
+        System.out.println("3.Exit");
+        while (isActive){
+
+        }
+    }
+}
